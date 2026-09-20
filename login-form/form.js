afde4100d16c970308dc163099e5a2a4fd01cc7d
Raunak <rounand44@gmail.com>
2026-09-20 23:51:45 +0530

    started js of login form

---
+++ login-form/form.js
@@ -0,0 +1,55 @@
+const form=document.getElementById("form");
+const firstNameInput=document.getElementById("firstname-input");
+const emailInput=document.getElementById("email");
+const passwordInput=document.getElementById("password");
+const repeatPasswordInput=document.getElementById("password-repeat");
+const errorMessage=document.getElementById("error-message");
+
+form.addEventListener("submit",(e)=>{
+  let errors=[];
+
+  if(firstName)
+  {
+    errors = getSignupFormErrors(firstNameInput.value,emailInput.value,passwordInput.value,repeatPasswordInput.value);
+  }
+  else
+  {
+    errors = getLoginFormErrors(email.value,password.value);
+  }
+
+  if(errors.length>0)
+  {
+      e.preventDefault();
+      errorMessage.innerText=errors.join(". ")
+  }
+});
+
+function getSignupFormErrors(firstName,email,password,repeatPassword){
+let errors=[];
+
+  if(firstName==='' || firstName===null)
+  {
+    errors.push("First name is required");
+    firstNameInput.parentElement.classList.add("incorrect")
+  }
+
+  if(email==='' || email===null)
+  {
+    errors.push("email is required");
+    emailInput.parentElement.classList.add("incorrect")
+  }
+
+  if(password==='' || password===null)
+  {
+    errors.push("password is required");
+    passwordInput.parentElement.classList.add("incorrect")
+  }
+
+  if(repeatPassword==='' || repeatPassword===null)
+  {
+    errors.push("Repeat password is required");
+    repeatPasswordInput.parentElement.classList.add("incorrect")
+  }
+
+  return errors;
+}
